b75c5101b159f611a0bf9b22d047c36ad8d3f219
HelloBug <148183740+1902231@users.noreply.github.com>
2025-01-15 22:27:17 +0800

    测试2

--- 2025GGJam/Assets/NewBehaviourScript.cs
+++ 2025GGJam/Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.log("haha");
+        Debug.Log("haha");
     }
 
     // Update is called once per frame
