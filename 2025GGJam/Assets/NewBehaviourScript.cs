2d6deb75d944cdec401157732a96a6ce9919a88f
Octopus33 <l2910410670@163.com>
2025-01-15 22:22:32 +0800

    更新 NewBehaviourScript.cs

--- 2025GGJam/Assets/NewBehaviourScript.cs
+++ 2025GGJam/Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.log("haha");
     }
 
     // Update is called once per frame
