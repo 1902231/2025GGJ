9c9d9e572b198b2be0a3c346a0fe15e619ebc6ab
Octopus33 <l2910410670@163.com>
2025-01-18 15:35:36 +0800

    1

---
+++ 2025GGJam/Assets/Scripts/MiddleBubbleAI.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddleBubbleAI : MonoBehaviour
+{
+    private Collider2D bubble;
+    private Transform bubble_Transform;
+    private float timer;
+
+
+    public Transform player_Transform;
+    public Rigidbody2D player_Rb;
+    public float explode_speed;
+    public float explode_waitTime;
+
+    public void Awake()
+    {
+        player_Transform = GameObject.Find("Pizza").transform;
+        player_Rb = GameObject.Find("Pizza").GetComponent<Rigidbody2D>();
+    }
+    public void Start()
+    {
+        bubble = GetComponent<Collider2D>();
+        bubble_Transform = GetComponent<Transform>();
+
+    }
+    public void Update()
+    {
+        Destroy();
+        Explode();
+        isTouchPlayer();
+    }
+
+    public void Explode()
+    {
+        
+        if (isTouchPlayer())
+        {
+
+            player_Rb.velocity = new Vector3(player_Rb.velocity.x, explode_speed, 0);
+            Destroy(this.gameObject);
+        }
+        
+
+
+        
+    }
+
+    public bool isTouchPlayer()
+    {
+        if (bubble.IsTouchingLayers(LayerMask.GetMask("Player")))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void Destroy()
+    {
+        timer += Time.deltaTime;
+        if (timer > explode_waitTime)
+        {
+
+            Destroy(this.gameObject);
+
+        }
+    }
+}
