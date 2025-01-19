46482654dfab1a38b1313ae642184a5880b83817
HelloBug <148183740+1902231@users.noreply.github.com>
2025-01-19 14:18:33 +0800

    合并5

---
+++ 2025GGJam/Assets/Scripts/Game/PizzaAnim.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PizzaAnim : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.J))
+        {
+            anim.SetBool("OnJ",true);
+        }
+        else
+        {
+            anim.SetBool("OnJ", false);
+        }
+    }
+
+}
